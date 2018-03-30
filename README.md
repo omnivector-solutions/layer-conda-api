0753560de6e3dc7930c9d72f81e6d3502c4ea546
jamesbeedy <jamesbeedy@gmail.com>
2018-03-30 07:52:06 -0700

    update readme

--- README.md
+++ README.md
@@ -8,25 +8,51 @@ The 'conda-api' exposes a few choice functionalities to charm authors.
 Include this layer in your charm, then import `from charms.layer import conda_api` to access the helper functions.
 
 ```python
-init_install_conda()
+init_install_conda(url, sha, validate)
 ```
 Installs a conda environment to `/opt/anaconda`.
 
+##### Example Usage:
 ```python
-create_conda_venv()
+conda_install_url =
+    "https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh"
+
+sha256 = "7e6785caad25e33930bc03fac4994a434a21bc8401817b7efa28f53619fa9c29"
+
+validate = "sha256"
+
+init_install_conda(conda_install_url, sha256, validate)
 ```
-Creates a conda venv named 'juju'.
 
 ```python
-install_conda_packages()
+create_conda_venv(python_version, packages=None)
 ```
-Installs a list of conda packages.
+Create conda venv and optionally install packages.
+
+##### Example Usage:
+```python
+create_conda_venv('3.5', ['jupyter', 'nb_conda'])
+```
 
 ```python
-install_conda_pip_packages()
+install_conda_packages(conda_packages)
+```
+Install conda packages.
+
+##### Example Usage:
+```python
+install_conda_packages(['jupyter', 'nb_conda'])
+```
+
+```python
+install_conda_pip_packages(conda_pip_packages)
 ```
 Installs a list of pip packages into your conda environment.
 
+##### Example Usage:
+```python
+install_conda_pip_packages(['findspark'])
+```
 
 ### License
 * AGPLv3 (see `copyright` file)
--- lib/charms/layer/conda_api.py
+++ lib/charms/layer/conda_api.py
@@ -14,31 +14,39 @@ CONDA_PIP_BIN = CONDA_HOME / 'envs' / 'juju' / 'bin' / 'pip'
 
 def init_install_conda(url, sha, validate):
     """Install conda
+
+    Example Usage:
+
+    conda_install_url =
+        "https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh"
+
+    sha256 = "7e6785caad25e33930bc03fac4994a434a21bc8401817b7efa28f53619fa9c29"
+
+    validate = "sha256"
+
+    init_install_conda(conda_install_url, sha256, validate)
     """
 
+    # Download and validate conda installer
     aufh = ArchiveUrlFetchHandler()
     conda_installer_path = aufh.download_and_validate(
         url,
         sha,
-        validate=validate
+        validate
     )
-
+    # If CONDA_HOME exists, remove it
     if CONDA_HOME.is_dir():
         shutil.rmtree(str(CONDA_HOME))
-
+    # Capabilities
     CONDA_HOME.mkdir(mode=0o777, parents=True)
-
     # Run conda installer
     call(['bash', conda_installer_path, '-b', '-f', '-p', str(CONDA_HOME)])
-
     # Make sure conda base is up to date
     call([str(CONDA_BIN), 'update', '-y', '-n', 'base', 'conda'])
 
 
 def create_conda_venv(python_version, packages=None):
     """Create conda venv and optionally install packages.
-
-    This can only be called one time.
 
     Example Usage:
 
@@ -49,7 +57,6 @@ def create_conda_venv(python_version, packages=None):
                          'python={}'.format(python_version)]
     if packages:
         create_conda_venv = create_conda_venv + packages
-
     call(create_conda_venv)
 
 
